cc8f0835cec3897768b71e012b9923df544902b3
Rohit <arohitu@gmail.com>
2018-09-19 10:28:45 +0530

    Added the initial version of components.

---
+++ aura/LEXDataTableStaticAction/LEXDataTableStaticActionController.js
@@ -0,0 +1,48 @@
+({
+    init: function (cmp, event, helper) {
+        var rowActions = helper.getRowActions.bind(this, cmp);
+        /*var actions = [
+            { label: 'Show details', name: 'show_details' },
+            { label: 'Delete', name: 'delete' }
+        ], 
+        fetchData = {
+            name : 'company.bsBuzz',
+            author: 'name.findName',
+            published : 'address.state'
+        };
+		*/
+
+        cmp.set('v.columns', [
+            { label: 'Name', fieldName: 'name', type: 'text' },
+            { label: 'Author', fieldName: 'author', type: 'text' },
+            { label: 'Publishing State', fieldName: 'published', type: 'text' },
+            { label: 'Action', type: 'action', typeAttributes: { rowActions: rowActions } }
+        ]);
+        
+        
+        cmp.set('v.data', [
+            { "name":"Adventures","author":"Rohit","published":"Printed"},
+            { "name":"Travelogue","author":"John","published":"Draft"},
+            { "name":"Food","author":"Simon","published":"In Review"},
+            { "name":"History","author":"Thomas","published":"Printed"},
+            { "name":"Adventures","author":"Jeni","published":"Draft"}
+        ]);
+    },
+
+    handleRowAction: function (cmp, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+
+        switch (action.name) {
+            case 'show_details':
+                alert('Showing Details: ' + JSON.stringify(row));
+                break;
+            case 'delete':
+                helper.removeBook(cmp, row);
+                break;
+        }
+    },
+    handleHeaderAction : function (cmp, event, helper) {
+        alert('Header');
+    }
+})
---
+++ aura/LEXDataTableStaticActionApp/LEXDataTableStaticActionApp.app
@@ -0,0 +1,5 @@
+<aura:application extends="force:slds" >
+    <div style="height:200px;border:1">
+        <c:LEXDataTableStaticAction/>
+    </div>
+</aura:application>
