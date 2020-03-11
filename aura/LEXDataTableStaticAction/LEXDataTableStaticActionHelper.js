b9c779fbb5d7820c70acae3240fb925c7a1ee4c9
arohitu <arohitu@gmail.com>
2020-03-11 13:37:53 +0530

    Update LEXDataTableStaticActionHelper.js

--- aura/LEXDataTableStaticAction/LEXDataTableStaticActionHelper.js
+++ aura/LEXDataTableStaticAction/LEXDataTableStaticActionHelper.js
@@ -2,31 +2,25 @@
     removeBook: function (cmp, row) {
         var rows = cmp.get('v.data');
         var rowIndex = rows.indexOf(row);
-
         rows.splice(rowIndex, 1);
         cmp.set('v.data', rows);
     },
     
     getRowActions : function (cmp, row, doneCallback) {
-        debugger;
-        var actions = [{
+        var actions = [];
+        var showdetailAction = {
             'label': 'Show Details',
             'iconName': 'utility:zoomin',
             'name': 'show_details'
-        }];
+        };
+        actions.push(showdetailAction);
         var deleteAction = {
             'label': 'Delete',
             'iconName': 'utility:delete',
             'name': 'delete'
         };
-        if (row['published'] === 'Printed') {
-            deleteAction['disabled'] = 'true';
+        if (row['published'] !== 'Printed') {
+            actions.push(deleteAction);
         } 
-        
-        actions.push(deleteAction);
-        // simulate a trip to the server
-        setTimeout($A.getCallback(function () {
-            doneCallback(actions);
-        }), 200);
     }
 })
